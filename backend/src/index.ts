7f1fee9b9a036d8026605964b40a4f832bc14c32
Nathan Cloutier <nathanc4571@gmail.com>
2025-10-26 11:55:49 -0400

    Proxy the frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/authRouter";
 import recipeRouter from "./routes/recipeRouter";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 dotenv.config();
 
@@ -15,6 +16,15 @@ app.use(cookieParser());
 app.use("/api/auth", authRouter);
 app.use("/api/recipes", recipeRouter);
 app.use("/api/images", express.static("uploads"));
+
+// Frontend
+app.use(
+  "/",
+  createProxyMiddleware({
+    target: "http://localhost:5173",
+    changeOrigin: true,
+  })
+);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
