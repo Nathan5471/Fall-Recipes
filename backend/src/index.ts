07a6f8797dbe82d533f84425858c1d79067a27cf
Nathan Cloutier <nathanc4571@gmail.com>
2025-10-26 12:30:29 -0400

    Fix mongoose

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
 import authRouter from "./routes/authRouter";
 import recipeRouter from "./routes/recipeRouter";
 import { createProxyMiddleware } from "http-proxy-middleware";
 
 dotenv.config();
+
+const MONGOOSE_URL = process.env.MONGOOSE_URL;
+
+if (!MONGOOSE_URL) {
+  throw new Error("MONGOOSE_URL is not defined in environment variables");
+}
 
 const app = express();
 
@@ -26,6 +33,13 @@ app.use(
   })
 );
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+mongoose
+  .connect(MONGOOSE_URL)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
